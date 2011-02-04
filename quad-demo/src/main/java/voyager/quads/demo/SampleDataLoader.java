0c2dea3f4454b068356a02cd17b5cd1da9d4f11c
Ryan McKinley <ryantxu@gmail.com>
2011-02-04 16:10:09 +0000

    load data

--- quad-demo/src/main/java/voyager/quads/demo/SampleDataLoader.java
+++ quad-demo/src/main/java/voyager/quads/demo/SampleDataLoader.java
@@ -6,6 +6,8 @@ import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.impl.StreamingUpdateSolrServer;
 import org.apache.solr.common.SolrInputDocument;
 
+import voyager.quads.utils.countries.CountryReader;
+import voyager.quads.utils.geoeye.GeoeyeReader;
 import voyager.quads.utils.geonames.Geoname;
 import voyager.quads.utils.geonames.GeonamesReader;
 
@@ -17,17 +19,17 @@ public class SampleDataLoader
     SolrServer solr = new StreamingUpdateSolrServer( "http://localhost:8080/solr", 50, 3 );
     //SolrServer solr = new CommonsHttpSolrServer( "http://localhost:8080/solr" );
 
-//    file = new File( "../data/ikonos_2011/ikonos_2011.shp" );
-//    if( file.exists() ) {
-//      GeoeyeReader.indexItems( solr, file );
-//      solr.commit( true, true );
-//    }
-//
-//    file = new File( "../data/countries/cntry06.shp" );
-//    if( file.exists() ) {
-//      CountryReader.indexCountries( solr, file );
-//      solr.commit( true, true );
-//    }
+    file = new File( "../data/ikonos_2011/ikonos_2011.shp" );
+    if( file.exists() ) {
+      GeoeyeReader.indexItems( solr, file );
+      solr.commit( true, true );
+    }
+
+    file = new File( "../data/countries/cntry06.shp" );
+    if( file.exists() ) {
+      CountryReader.indexCountries( solr, file );
+      solr.commit( true, true );
+    }
 
     // Geonames
     file = new File( "../data/geonames/US.txt" );
