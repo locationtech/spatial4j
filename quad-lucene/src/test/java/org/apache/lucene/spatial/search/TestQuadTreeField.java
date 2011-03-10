f42d592b330d654e1a74536e891b34511b2266af
Ryan McKinley <ryantxu@gmail.com>
2011-03-10 19:21:00 +0000

    adding package

---
+++ quad-lucene/src/test/java/org/apache/lucene/spatial/search/TestQuadTreeField.java
@@ -0,0 +1,15 @@
+package org.apache.lucene.spatial.search;
+
+import junit.framework.TestCase;
+
+
+
+/**
+ */
+public class TestQuadTreeField extends TestCase
+{
+  public void testMatchInfo() throws Exception
+  {
+    // TODO
+  }
+}
