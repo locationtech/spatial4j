b32aebe19491c7b10e2f685d7c870e83240938cb
Ryan McKinley <ryantxu@gmail.com>
2011-04-25 19:00:36 +0000

    adding strategy tests with JTS

---
+++ spatial-extras-test/src/test/java/org/googlecode/lucene/spatial/test/strategy/ExtrasGeohashStrategyTestCase.java
@@ -0,0 +1,15 @@
+package org.googlecode.lucene.spatial.test.strategy;
+
+import org.apache.lucene.spatial.base.context.SpatialContext;
+import org.apache.lucene.spatial.test.strategy.BaseGeohashStrategyTestCase;
+
+import com.googlecode.lucene.spatial.base.context.JtsSpatialContext;
+
+
+public class ExtrasGeohashStrategyTestCase extends BaseGeohashStrategyTestCase {
+
+  @Override
+  protected SpatialContext getSpatialContext() {
+    return new JtsSpatialContext();
+  }
+}
