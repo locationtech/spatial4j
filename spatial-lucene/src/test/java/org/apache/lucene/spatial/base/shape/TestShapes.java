b4b13222b4c4181d6105e5368e606c5226a4123a
David Smiley <dsmiley@mitre.org>
2011-11-09 23:09:08 +0000

    Added ASF headers to spatial-lucene & spatial-solr.  Added LICENSE.txt files; no NOTICE yet.

--- spatial-lucene/src/test/java/org/apache/lucene/spatial/base/shape/TestShapes.java
+++ spatial-lucene/src/test/java/org/apache/lucene/spatial/base/shape/TestShapes.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.lucene.spatial.base.shape;
 
 import org.apache.lucene.spatial.base.context.SpatialContext;
@@ -30,8 +47,8 @@ public class TestShapes {
 
     //test equals & hashcode
     Point pt2 = ctx.makePoint(0,0);
-    assertEquals(msg,pt,pt2);
-    assertEquals(msg,pt.hashCode(),pt2.hashCode());
+    assertEquals(msg, pt, pt2);
+    assertEquals(msg, pt.hashCode(), pt2.hashCode());
 
     assertFalse(msg,pt.hasArea());
     assertEquals(msg,pt.getCenter(),pt);
