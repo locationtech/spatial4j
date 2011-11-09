9cf94af10fe0afb86cd328a1ce3ecac14d7c9e03
David Smiley <dsmiley@mitre.org>
2011-11-09 22:24:23 +0000

    Added spatial-solr tests ported from Solr's spatial tests.

---
+++ spatial-solr/src/test/java/org/apache/solr/spatial/SpatialFilterTest.java
@@ -0,0 +1,136 @@
+package org.apache.solr.spatial;
+
+
+import org.apache.lucene.spatial.base.distance.DistanceUtils;
+import org.apache.solr.SolrTestCaseJ4;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Based off of Solr's SpatialFilterTest.
+ */
+public class SpatialFilterTest extends SolrTestCaseJ4 {
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    initCore("solrconfig.xml", "schema.xml");
+  }
+
+  @Test
+  public void testRecursiveField() throws Exception {
+    testField("recursive");
+  }
+
+  //TODO test other field types
+
+  private void setupDocs(String fieldName) {
+    clearIndex();
+    assertU(adoc("id", "1", fieldName, "32.7693246, -79.9289094"));
+    assertU(adoc("id", "2", fieldName, "33.7693246, -80.9289094"));
+    assertU(adoc("id", "3", fieldName, "-32.7693246, 50.9289094"));
+    assertU(adoc("id", "4", fieldName, "-50.7693246, 60.9289094"));
+    assertU(adoc("id", "5", fieldName, "0,0"));
+    assertU(adoc("id", "6", fieldName, "0.1,0.1"));
+    assertU(adoc("id", "7", fieldName, "-0.1,-0.1"));
+    assertU(adoc("id", "8", fieldName, "0,179.9"));
+    assertU(adoc("id", "9", fieldName, "0,-179.9"));
+    assertU(adoc("id", "10", fieldName, "89.9,50"));
+    assertU(adoc("id", "11", fieldName, "89.9,-130"));
+    assertU(adoc("id", "12", fieldName, "-89.9,50"));
+    assertU(adoc("id", "13", fieldName, "-89.9,-130"));
+    assertU(commit());
+  }
+
+  private void testField(String fieldName) throws Exception {
+    setupDocs(fieldName);
+    //Try some edge cases
+    checkHits(fieldName, "1,1", 175, 3, 5, 6, 7);
+    checkHits(fieldName, "0,179.8", 200, 2, 8, 9);
+    checkHits(fieldName, "89.8, 50", 200, 2, 10, 11);//this goes over the north pole
+    checkHits(fieldName, "-89.8, 50", 200, 2, 12, 13);//this goes over the south pole
+    //try some normal cases
+    checkHits(fieldName, "33.0,-80.0", 300, 2);
+    //large distance
+    checkHits(fieldName, "1,1", 5000, 3, 5, 6, 7);
+    //Because we are generating a box based on the west/east longitudes and the south/north latitudes, which then
+    //translates to a range query, which is slightly more inclusive.  Thus, even though 0.0 is 15.725 kms away,
+    //it will be included, b/c of the box calculation.
+    checkHits(fieldName, false, "0.1,0.1", 15, 2, 5, 6);
+    //try some more
+    clearIndex();
+    assertU(adoc("id", "14", fieldName, "0,5"));
+    assertU(adoc("id", "15", fieldName, "0,15"));
+    //3000KM from 0,0, see http://www.movable-type.co.uk/scripts/latlong.html
+    assertU(adoc("id", "16", fieldName, "18.71111,19.79750"));
+    assertU(adoc("id", "17", fieldName, "44.043900,-95.436643"));
+    assertU(commit());
+
+    checkHits(fieldName, "0,0", 1000, 1, 14);
+    checkHits(fieldName, "0,0", 2000, 2, 14, 15);
+    checkHits(fieldName, false, "0,0", 3000, 3, 14, 15, 16);
+    checkHits(fieldName, "0,0", 3001, 3, 14, 15, 16);
+    checkHits(fieldName, "0,0", 3000.1, 3, 14, 15, 16);
+
+    //really fine grained distance and reflects some of the vagaries of how we are calculating the box
+    checkHits(fieldName, "43.517030,-96.789603", 109, 0);
+
+    // falls outside of the real distance, but inside the bounding box
+    checkHits(fieldName, true, "43.517030,-96.789603", 110, 0);
+    checkHits(fieldName, false, "43.517030,-96.789603", 110, 1, 17);
+  }
+
+  private void checkHits(String fieldName, String pt, double distance, int count, int ... docIds) {
+    checkHits(fieldName, true, pt, distance, count, docIds);
+  }
+
+  private void checkHits(String fieldName, boolean exact, String pt, double distance, int count, int ... docIds) {
+    String [] tests = new String[docIds != null && docIds.length > 0 ? docIds.length + 1 : 1];
+    tests[0] = "*[count(//doc)=" + count + "]";
+    if (docIds != null && docIds.length > 0) {
+      int i = 1;
+      for (int docId : docIds) {
+        tests[i++] = "//result/doc/*[@name='id'][.='" + docId + "']";
+      }
+    }
+
+    String method = exact ? "IsWithin" : "BBoxWithin";
+    double[] latLon = DistanceUtils.parseLatitudeLongitude(pt);
+
+    assertQ(req(
+          "fl", "id", "q","*:*", "rows", "1000", "fq",
+          "{! }"+fieldName+":\""+method+"(Circle("+latLon[1]+" "+latLon[0]+" d="+distance+"))\""),
+        tests);
+  }
+
+
+}
+/*public void testSpatialQParser() throws Exception {
+  ModifiableSolrParams local = new ModifiableSolrParams();
+  local.add(CommonParams.FL, "home");
+  ModifiableSolrParams params = new ModifiableSolrParams();
+  params.add(SpatialParams.POINT, "5.0,5.0");
+  params.add(SpatialParams.DISTANCE, "3");
+  SolrQueryRequest req = new LocalSolrQueryRequest(h.getCore(), "", "", 0, 10, new HashMap());
+  SpatialFilterQParserPlugin parserPlugin;
+  Query query;
+
+  parserPlugin = new SpatialFilterQParserPlugin();
+  QParser parser = parserPlugin.createParser("'foo'", local, params, req);
+  query = parser.parse();
+  assertNotNull("Query is null", query);
+  assertTrue("query is not an instanceof "
+          + BooleanQuery.class,
+          query instanceof BooleanQuery);
+  local = new ModifiableSolrParams();
+  local.add(CommonParams.FL, "x");
+  params = new ModifiableSolrParams();
+  params.add(SpatialParams.POINT, "5.0");
+  params.add(SpatialParams.DISTANCE, "3");
+  req = new LocalSolrQueryRequest(h.getCore(), "", "", 0, 10, new HashMap());
+  parser = parserPlugin.createParser("'foo'", local, params, req);
+  query = parser.parse();
+  assertNotNull("Query is null", query);
+  assertTrue(query.getClass() + " is not an instanceof "
+          + NumericRangeQuery.class,
+          query instanceof NumericRangeQuery);
+  req.close();
+}*/
--- spatial-solr/src/test/java/org/apache/solr/spatial/TestSolrSpatialField.java
+++ spatial-solr/src/test/java/org/apache/solr/spatial/TestSolrSpatialField.java
@@ -1,15 +1,78 @@
 package org.apache.solr.spatial;
 
-import junit.framework.TestCase;
-
+import org.apache.solr.SolrTestCaseJ4;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
 
 
 /**
  */
-public class TestSolrSpatialField extends TestCase
+public class TestSolrSpatialField extends SolrTestCaseJ4
 {
-  public void testMatchInfo() throws Exception
-  {
-    // TODO
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    initCore("solrconfig.xml", "schema.xml");
+  }
+
+  /** Ported from DistanceFunctionTest */
+  @Test @Ignore
+  public void ported_DistanceFunctionTest_testLatLon() throws Exception {
+    final String sfield = "recursive";
+    assertU(adoc("id", "100", sfield, "1,2"));
+    assertU(commit());
+
+    // default to reading pt
+    assertJQ(req("defType","func",
+        "q","geodist(1,2)",
+        "pt","3,4",
+        "fq","id:100",
+        "fl","id,score")
+        , 1e-5
+        , "/response/docs/[0]/score==314.40338"
+    );
+
+    // default to reading pt first
+    assertJQ(req("defType","func",
+        "q","geodist(1,2)",
+        "pt","3,4",
+        "sfield", sfield,
+        "fq","id:100",
+        "fl","id,score")
+        , 1e-5
+        , "/response/docs/[0]/score==314.40338"
+    );
+
+    // if pt missing, use sfield
+    assertJQ(req("defType","func",
+        "q","geodist(3,4)",
+        "sfield", sfield,
+        "fq","id:100",
+        "fl","id,score")
+        , 1e-5
+        ,"/response/docs/[0]/score==314.40338"
+    );
+
+    // read both pt and sfield
+    assertJQ(req("defType","func",
+        "q","geodist()","pt","3,4",
+        "sfield", sfield,
+        "fq","id:100",
+        "fl","id,score")
+        , 1e-5
+        ,"/response/docs/[0]/score==314.40338"
+    );
+
+    // param substitution
+    assertJQ(req("defType","func",
+        "q","geodist($a,$b)",
+        "a","3,4",
+        "b", sfield,
+        "fq","id:100",
+        "fl","id,score")
+        , 1e-5
+        ,"/response/docs/[0]/score==314.40338"
+    );
+
   }
 }
