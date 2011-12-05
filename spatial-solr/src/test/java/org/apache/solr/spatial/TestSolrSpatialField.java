ee7220ff277fa737d83a2ab7bb0f2faa4c4a5115
David Smiley <dsmiley@mitre.org>
2011-12-05 17:12:31 +0000

    Test sorting via Solr.  More tests needed to verify multi-value point sort.  The testing schema.xml needed version=1.4

--- spatial-solr/src/test/java/org/apache/solr/spatial/TestSolrSpatialField.java
+++ spatial-solr/src/test/java/org/apache/solr/spatial/TestSolrSpatialField.java
@@ -32,6 +32,53 @@ public class TestSolrSpatialField extends SolrTestCaseJ4
     initCore("solrconfig.xml", "schema.xml");
   }
 
+  @Test
+  public void testScoreDistance_directQuery() throws Exception {
+    final String sfield = "recursive";
+    assertU(adoc("id", "100", sfield, "1,2"));
+    assertU(adoc("id", "101", sfield, "4,-1"));
+    assertU(commit());
+
+    assertJQ(req(
+        "q",sfield+":\"Intersects(Circle(3,4 d=1000))\"",
+        "fl","id,score",
+        "sort","score asc")//want ascending due to increasing distance
+        , 1e-4
+        , "/response/docs/[0]/id=='100'"
+        , "/response/docs/[0]/score==314.4034"
+        , "/response/docs/[1]/id=='101'"
+        , "/response/docs/[1]/score==565.9612"
+    );
+    //query again with the query point closer to #101, and check the new ordering
+    assertJQ(req(
+        "q",sfield+":\"Intersects(Circle(4,0 d=1000))\"",
+        "fl","id,score",
+        "sort","score asc")//want ascending due to increasing distance
+        , 1e-4
+        , "/response/docs/[0]/id=='101'"
+        , "/response/docs/[1]/id=='100'"
+    );
+
+    //use sort=query(...)
+    assertJQ(req(
+        "q","*:*",
+        "sort","query($sortQuery) asc", //want ascending due to increasing distance
+        "sortQuery",sfield+":\"Intersects(Circle(3,4 d=1000))\"",
+        "fl","id,score")
+        , 1e-4
+        , "/response/docs/[0]/id=='100'"
+        , "/response/docs/[1]/id=='101'"  );
+    //check reversed direction with query point closer to #101
+    assertJQ(req(
+        "q","*:*",
+        "sort","query($sortQuery) asc", //want ascending due to increasing distance
+        "sortQuery",sfield+":\"Intersects(Circle(4,0 d=1000))\"",
+        "fl","id,score")
+        , 1e-4
+        , "/response/docs/[0]/id=='101'"
+        , "/response/docs/[1]/id=='100'"  );
+  }
+
   /** Ported from DistanceFunctionTest */
   @Test @Ignore
   public void ported_DistanceFunctionTest_testLatLon() throws Exception {
