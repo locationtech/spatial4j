e245d95da05821ff0ca3a01071672e5b081d822c
David Smiley <dsmiley@mitre.org>
2012-11-27 14:37:19 -0500

    Added relateContainsShortCircuits() with default of true (new behavior; effectively false prior), and more documentation.

--- src/main/java/com/spatial4j/core/shape/ShapeCollection.java
+++ src/main/java/com/spatial4j/core/shape/ShapeCollection.java
@@ -32,16 +32,22 @@ import static com.spatial4j.core.shape.SpatialRelation.INTERSECTS;
  * A collection of Shape objects, analogous to an OGC GeometryCollection. The
  * implementation demands a List (with random access) so that the order can be
  * retained if an application requires it, although logically it's treated as an
- * unordered Set.  Consequently, {@link #relate(Shape)} should return the same
- * result no matter what the shape order is. There is no restriction on whether
- * the shapes overlap each other at all. As the Shape contract states; it may
- * return intersects when the best answer is actually contains or within. If
- * any shape intersects the provided shape then that is the answer.
+ * unordered Set, mostly.
+ * <p/>
+ * Ideally, {@link #relate(Shape)} should return the same result no matter what
+ * the shape order is, although the default implementation can be order
+ * dependent when the shapes overlap; see {@link #relateContainsShortCircuits()}.
+ *  To improve performance slightly, the caller could order the shapes by
+ * largest first so that relate() will have a greater chance of
+ * short-circuit'ing sooner.  As the Shape contract states; it may return
+ * intersects when the best answer is actually contains or within. If any shape
+ * intersects the provided shape then that is the answer.
+ * <p/>
+ * This implementation is not optimized for a large number of shapes; relate is
+ * O(N).  A more sophisticated implementation might do an R-Tree based on
+ * bbox'es, for example.
  */
 public class ShapeCollection<S extends Shape> extends AbstractList<S> implements Shape {
-
-  //TODO add option to detect that all shapes are mutually disjoint. If that is
-  // the case then relate() can return immediately if it finds a CONTAINS.
 
   protected final List<S> shapes;
   protected final Rectangle bbox;
@@ -119,7 +125,9 @@ public class ShapeCollection<S extends Shape> extends AbstractList<S> implements
     if (bboxSect == SpatialRelation.DISJOINT || bboxSect == SpatialRelation.WITHIN)
       return bboxSect;
 
-    SpatialRelation sect = null;//CONTAINS, WITHIN, or DISJOINT
+    final boolean containsWillShortCircuit = (other instanceof Point) ||
+        relateContainsShortCircuits();
+    SpatialRelation sect = null;
     for (Shape shape : shapes) {
       SpatialRelation nextSect = shape.relate(other);
 
@@ -131,13 +139,51 @@ public class ShapeCollection<S extends Shape> extends AbstractList<S> implements
 
       if (sect == INTERSECTS)
         return INTERSECTS;
-      if (sect == CONTAINS) {
-        if (other instanceof Point)
-          return CONTAINS;
-        //TODO if mutually disjoint (cached in a bool flag), return CONTAINS
-      }
+
+      if (sect == CONTAINS && containsWillShortCircuit)
+        return CONTAINS;
     }
     return sect;
+  }
+
+  /**
+   * Called by relate() to determine whether to return early if it finds
+   * CONTAINS, instead of checking the remaining shapes. It will do so without
+   * calling this method if the "other" shape is a Point.  If a remaining shape
+   * finds INTERSECTS, then INTERSECTS will be returned.  The only problem with
+   * this returning true is that if some of the shapes overlap, it's possible
+   * that the result of relate() could be dependent on the order of the shapes,
+   * which could be unexpected / wrong depending on the application. The default
+   * implementation returns true because it probably doesn't matter.  If it
+   * does, a subclass could add a boolean flag that this method could return.
+   * That flag could be initialized to true only if the shapes are mutually
+   * disjoint.
+   *
+   * @see #computeMutualDisjoint(java.util.List) .
+   */
+  protected boolean relateContainsShortCircuits() {
+    return true;
+  }
+
+  /**
+   * Computes whether the shapes are mutually disjoint. This is a utility method
+   * offered for use by a subclass implementing {@link #relateContainsShortCircuits()}.
+   * <b>Beware: this is an O(N^2) algorithm.</b>.  Consequently, consider safely
+   * assuming non-disjoint if shapes.size() > 10 or something.  And if all shapes
+   * are a Point then the result of this method doesn't ultimately matter.
+   */
+  protected static boolean computeMutualDisjoint(List<? extends Shape> shapes) {
+    //WARNING: this is an O(n^2) algorithm.
+    //loop through each shape and see if it intersects any shape before it
+    for (int i = 1; i < shapes.size(); i++) {
+      Shape shapeI = shapes.get(i);
+      for (int j = 0; j < i; j++) {
+        Shape shapeJ = shapes.get(j);
+        if (shapeJ.relate(shapeI).intersects())
+          return false;
+      }
+    }
+    return true;
   }
 
   @Override
