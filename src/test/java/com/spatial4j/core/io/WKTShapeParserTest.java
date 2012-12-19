138557fd94ee652701704f7f904ee85238061c4c
David Smiley <dsmiley@mitre.org>
2012-12-19 11:37:47 -0500

    Add WKT tests (partially ported from ElasticSearch).  There needs to be more.

---
+++ src/test/java/com/spatial4j/core/io/WKTShapeParserTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.spatial4j.core.io;
+
+import com.carrotsearch.randomizedtesting.RandomizedTest;
+import com.spatial4j.core.context.SpatialContext;
+import com.spatial4j.core.shape.Shape;
+import org.junit.Test;
+
+import java.text.ParseException;
+
+public class WKTShapeParserTest extends RandomizedTest {
+  SpatialContext ctx = SpatialContext.GEO;
+  WKTShapeParser SHAPE_PARSER = new WKTShapeParser(ctx);
+
+  protected void assertParses(String wkt, Shape expected) throws ParseException {
+    assertEquals(SHAPE_PARSER.parse(wkt), expected);
+  }
+
+  protected void assertFails(String wkt) {
+    try {
+      SHAPE_PARSER.parse(wkt);
+      fail("ParseException expected");
+    } catch (ParseException e) {//expected
+    }
+  }
+
+  @Test
+  public void testNoOp() throws ParseException {
+    assertEquals(null, SHAPE_PARSER.parseIfSupported(""));
+    assertEquals(null, SHAPE_PARSER.parseIfSupported("  "));
+    assertEquals(null, SHAPE_PARSER.parseIfSupported("TestShape()"));
+  }
+
+  @Test
+  public void testParsePoint() throws ParseException {
+    assertParses("POINT (100 90)", ctx.makePoint(100, 90));//typical
+    assertParses(" POINT (100 90) ", ctx.makePoint(100, 90));//trimmed
+    assertParses("point (100 90)", ctx.makePoint(100, 90));//case indifferent
+    assertParses("POINT ( 100 90 )", ctx.makePoint(100, 90));//inner spaces
+    assertParses("POINT(100 90)", ctx.makePoint(100, 90));
+    assertParses("POINT (-45 90 )", ctx.makePoint(-45, 90));
+    assertParses("POINT (-45.3 80.4 )", ctx.makePoint(-45.3, 80.4));
+  }
+
+  @Test
+  public void testParsePoint_invalidDefinitions() {
+    assertFails("POINT 100 90");
+    assertFails("POINT (100 90");
+    assertFails("POINT 100 90)");
+    assertFails("POINT (100)");
+    assertFails("POINT (10f0 90)");
+  }
+
+}
