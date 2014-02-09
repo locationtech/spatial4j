b498c35d126c857175ff7bdafcc5e73430a3ca68
Rebecca Alford <rfalford12@gmail.com>
2014-02-08 16:47:21 -0800

    Updating geo graph utils and geograph comments

--- src/main/java/com/spatial4j/core/shape/graph/GeoGraph.java
+++ src/main/java/com/spatial4j/core/shape/graph/GeoGraph.java
@@ -1,11 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.spatial4j.core.shape.graph;
 
+import com.spatial4j.core.shape.Point;
+
 /**
- * Created with IntelliJ IDEA.
- * User: rfalford12
- * Date: 2/8/14
- * Time: 3:52 PM
- * To change this template use File | Settings | File Templates.
+ * GeoGraph - Represents points in space connected by a geodesic distance
  */
 public class GeoGraph {
+
+    // maybe store a spatial context?
+    private GeoEdge[] edgeList;
+
+    private GeoGraph() {}
+
+    public GeoGraph( Point[] points ) {
+        init(points);
+    }
+
+    /**
+     * Given a list of ordered points, create an edge for the pairs of points
+     * in order creating a graph.
+     * @param points
+     */
+    private init( Point[] points ) {
+
+
+    }
+
+    /**
+     * Is valid geograph
+     * DAG invariants - mostly due to search invariants
+     */
+
+    /**
+     * Return unique point set
+     */
+
+
+
 }
