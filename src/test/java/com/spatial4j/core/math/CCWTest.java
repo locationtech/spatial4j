42032ea2e4579f43bc04267268a8c3d9c431b161
Rebecca Alford <rfalford12@gmail.com>
2014-03-30 16:09:35 -0400

    Adding algorithm for randomized CCW testing - should be able to add ot the actual tests soon> Running into internal assert issues with expensive ccw will need to do this too

--- src/test/java/com/spatial4j/core/math/CCWTest.java
+++ src/test/java/com/spatial4j/core/math/CCWTest.java
@@ -17,6 +17,12 @@
 
 package com.spatial4j.core.math;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import com.spatial4j.core.distance.DistanceUtils;
+import com.spatial4j.core.shape.Point;
+import com.spatial4j.core.shape.impl.PointImpl;
 import com.spatial4j.core.math.CCW;
 import com.spatial4j.core.math.Vector3DUtils;
 import com.spatial4j.core.shape.Vector3D;
@@ -55,13 +61,14 @@ public class CCWTest extends RandomizedTest {
     /**
      * Test Planar Ordered CCW Method
      */
+    @Test
     public void testPlanarOrderedCCW() {
-
     }
 
     /**
      * Test Planar CCW Method
      */
+    @Test
     public void testPlanarCCW() {
 
     }
@@ -69,6 +76,7 @@ public class CCWTest extends RandomizedTest {
     /**
      * Test Ordered CCW Method
      */
+    @Test
     public void testOrderedCCW() {
 
     }
@@ -76,7 +84,14 @@ public class CCWTest extends RandomizedTest {
     /**
      * Test Expensive CCW Method
      */
+    @Test
     public void testExpensiveCCW() {
+
+        // Test Case from S2 Lib
+        Vector3D a = new Vector3D(0.72571927877036835, 0.46058825605889098, 0.51106749730504852);
+        Vector3D b = new Vector3D(0.7257192746638208, 0.46058826573818168, 0.51106749441312738);
+        Vector3D c = new Vector3D(0.72571927671709457, 0.46058826089853633, 0.51106749585908795);
+       // assertTrue(CCW.expensiveCCW(a, b, c) != 0);
 
     }
 
@@ -90,6 +105,9 @@ public class CCWTest extends RandomizedTest {
         Vector3D a = new Vector3D(0.72571927877036835, 0.46058825605889098, 0.51106749730504852);
         Vector3D b = new Vector3D(0.7257192746638208, 0.46058826573818168, 0.51106749441312738);
         Vector3D c = new Vector3D(0.72571927671709457, 0.46058826089853633, 0.51106749585908795);
+
+        // Check CCW returns true
+        assertTrue(CCW.robustCCW(a, b, c, Vector3DUtils.crossProduct(a, b)) != 0);
         assertTrue(CCW.robustCCW(a, b, c) != 0);
 
     }
@@ -99,10 +117,45 @@ public class CCWTest extends RandomizedTest {
      */
     private Vector3D[] make_random_point_set( int numPoints, boolean oriented, boolean ccw ) {
 
-        // Create a New Random GeoCircle (just needs to be a valid circle)
-        ctx.makeCircle(45, 45, 10);
+       // Picking a Non Random Lat Lon Point to start (boston -
+        double x = 0; // some rand coord
+        double y = 0; // some rand coord
+        double r = 10; // fixed radius - doesn't matter becasue we will use direction vectors anyway
 
-        // Split the circle into random segments
+
+        // just kidding - do we even need the circle? its just a container for information
+        // I already have
+
+        // Pick four random points in the quadrants of the circle
+        List< Point > pointArr = new ArrayList< Point >(4);
+
+        // Quadrant 1
+        double alpha = randomAngle(0.0, 90.0);
+        Point a = new PointImpl( r*Math.cos(alpha), r*Math.sin(alpha), ctx);
+        pointArr.add(a);
+
+        // Quadrant 2
+        double beta = randomAngle(90.0, 180.0);
+        Point b = new PointImpl( r*Math.cos(beta), r*Math.sin(beta), ctx);
+        pointArr.add(b);
+
+        // Quadrant 3
+        double gamma = randomAngle(180.0, 270.0);
+        Point c = new PointImpl( r*Math.cos(gamma), r*Math.sin(gamma), ctx);
+        pointArr.add(c);
+
+        // Quadrant 4
+        double chi = randomAngle(270.0, 360.0);
+        Point d = new PointImpl( r*Math.cos(chi), r*Math.sin(chi), ctx);
+        pointArr.add(d);
+
+        // Declare 3 indices
+
+
+        // If no orientation, pick three random indices
+        if ( !oriented ) {
+
+        }
 
         // pick random points on the circle in each bin
 
@@ -110,8 +163,16 @@ public class CCWTest extends RandomizedTest {
 
         // return point set
 
+         return null;
 
+    }
 
+    /**
+     * Helper method - pick a random angle within range and convert to degrees
+     */
+    double randomAngle( double lower, double upper ) {
+        double angle = lower + (Math.random() * (upper - lower + 1));
+        return DistanceUtils.toRadians(angle);
     }
 
 }
