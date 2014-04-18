f5c1a133cbacbdd921dcf4473df901ec1555603a
Rebecca Alford <rfalford12@gmail.com>
2014-04-18 17:55:00 -0400

    Implementing geo range base class and real geo range subclass. Still working on unit range class and documentaiton for these

--- src/main/java/com/spatial4j/core/shape/GeoRange.java
+++ src/main/java/com/spatial4j/core/shape/GeoRange.java
@@ -15,18 +15,20 @@
  * limitations under the License.
  */
 
-package com.spatial4j.core.shape.impl;
-
-import com.spatial4j.core.context.SpatialContext;
-import com.spatial4j.core.shape.Rectangle;
+package com.spatial4j.core.shape;
 
 /**
  * Geodesic Range (Generic)
  */
 public class GeoRange {
 
-    protected final double min;
-    protected final double max;
+    protected double min;
+    protected double max;
+
+    public GeoRange() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     public GeoRange( double min, double max ) {
         this.min = min;
@@ -71,163 +73,6 @@ public class GeoRange {
     }
 }
 
-public class RealGeoRange extends Range {
-
-    /**
-     * Returns an empty interval. (Any interval where lo > hi is considered
-     * empty.)
-     */
-    public static R1Interval empty() {
-        return new R1Interval(1, 0);
-    }
-
-    /**
-     * Convenience method to construct an interval containing a single point.
-     */
-    public static R1Interval fromPoint(double p) {
-        return new R1Interval(p, p);
-    }
-
-    /**
-     * Convenience method to construct the minimal interval containing the two
-     * given points. This is equivalent to starting with an empty interval and
-     * calling AddPoint() twice, but it is more efficient.
-     */
-    public static R1Interval fromPointPair(double p1, double p2) {
-        if (p1 <= p2) {
-            return new R1Interval(p1, p2);
-        } else {
-            return new R1Interval(p2, p1);
-        }
-    }
-
-    public double lo() {
-        return lo;
-    }
-
-    public double hi() {
-        return hi;
-    }
-
-    /**
-     * Return true if the interval is empty, i.e. it contains no points.
-     */
-    public boolean isEmpty() {
-        return lo() > hi();
-    }
-
-    /**
-     * Return the center of the interval. For empty intervals, the result is
-     * arbitrary.
-     */
-    public double getCenter() {
-        return 0.5 * (lo() + hi());
-    }
-
-    /**
-     * Return the length of the interval. The length of an empty interval is
-     * negative.
-     */
-    public double getLength() {
-        return hi() - lo();
-    }
-
-    public boolean contains(double p) {
-        return p >= lo() && p <= hi();
-    }
-
-    public boolean interiorContains(double p) {
-        return p > lo() && p < hi();
-    }
-
-    /** Return true if this interval contains the interval 'y'. */
-    public boolean contains(R1Interval y) {
-        if (y.isEmpty()) {
-            return true;
-        }
-        return y.lo() >= lo() && y.hi() <= hi();
-    }
-
-    /**
-     * Return true if the interior of this interval contains the entire interval
-     * 'y' (including its boundary).
-     */
-    public boolean interiorContains(R1Interval y) {
-        if (y.isEmpty()) {
-            return true;
-        }
-        return y.lo() > lo() && y.hi() < hi();
-    }
-
-    /**
-     * Return true if this interval intersects the given interval, i.e. if they
-     * have any points in common.
-     */
-    public boolean intersects(R1Interval y) {
-        if (lo() <= y.lo()) {
-            return y.lo() <= hi() && y.lo() <= y.hi();
-        } else {
-            return lo() <= y.hi() && lo() <= hi();
-        }
-    }
-
-    /**
-     * Return true if the interior of this interval intersects any point of the
-     * given interval (including its boundary).
-     */
-    public boolean interiorIntersects(R1Interval y) {
-        return y.lo() < hi() && lo() < y.hi() && lo() < hi() && y.lo() <= y.hi();
-    }
-
-    /** Expand the interval so that it contains the given point "p". */
-    public R1Interval addPoint(double p) {
-        if (isEmpty()) {
-            return R1Interval.fromPoint(p);
-        } else if (p < lo()) {
-            return new R1Interval(p, hi());
-        } else if (p > hi()) {
-            return new R1Interval(lo(), p);
-        } else {
-            return new R1Interval(lo(), hi());
-        }
-    }
-
-    /**
-     * Return an interval that contains all points with a distance "radius" of a
-     * point in this interval. Note that the expansion of an empty interval is
-     * always empty.
-     */
-    public R1Interval expanded(double radius) {
-        // assert (radius >= 0);
-        if (isEmpty()) {
-            return this;
-        }
-        return new R1Interval(lo() - radius, hi() + radius);
-    }
-
-    /**
-     * Return the smallest interval that contains this interval and the given
-     * interval "y".
-     */
-    public R1Interval union(R1Interval y) {
-        if (isEmpty()) {
-            return y;
-        }
-        if (y.isEmpty()) {
-            return this;
-        }
-        return new R1Interval(Math.min(lo(), y.lo()), Math.max(hi(), y.hi()));
-    }
-
-    /**
-     * Return the intersection of this interval with the given interval. Empty
-     * intervals do not need to be special-cased.
-     */
-    public R1Interval intersection(R1Interval y) {
-        return new R1Interval(Math.max(lo(), y.lo()), Math.min(hi(), y.hi()));
-    }
-
-}
 
 public class UnitGeoRange extends Range {
 
