cf79641684a9fa31f5edb1830b8d38fe379039a6
Rebecca Alford <rfalford12@gmail.com>
2014-04-27 13:54:51 -0400

    Adding starter code for JTS binary trees - much of this is started but still needs to be adapted and documentation added

--- src/main/java/com/spatial4j/core/algorithm/index/JTSBinTree.java
+++ src/main/java/com/spatial4j/core/algorithm/index/JTSBinTree.java
@@ -48,4 +48,123 @@ public class JTSBinTree {
         if ( )
     }
 
+    private Root root;
+    /**
+     *  Statistics
+     *
+     * minExtent is the minimum extent of all items
+     * inserted into the tree so far. It is used as a heuristic value
+     * to construct non-zero extents for features with zero extent.
+     * Start with a non-zero extent, in case the first feature inserted has
+     * a zero extent in both directions.  This value may be non-optimal, but
+     * only one feature will be inserted with this value.
+     **/
+    private double minExtent = 1.0;
+
+    public Bintree()
+    {
+        root = new Root();
+    }
+
+    public int depth()
+    {
+        if (root != null) return root.depth();
+        return 0;
+    }
+    public int size()
+    {
+        if (root != null) return root.size();
+        return 0;
+    }
+    /**
+     * Compute the total number of nodes in the tree
+     *
+     * @return the number of nodes in the tree
+     */
+    public int nodeSize()
+    {
+        if (root != null) return root.nodeSize();
+        return 0;
+    }
+
+    public void insert(Interval itemInterval, Object item)
+    {
+        collectStats(itemInterval);
+        Interval insertInterval = ensureExtent(itemInterval, minExtent);
+//int oldSize = size();
+        root.insert(insertInterval, item);
+    /* DEBUG
+int newSize = size();
+System.out.println("BinTree: size = " + newSize + "   node size = " + nodeSize());
+if (newSize <= oldSize) {
+      System.out.println("Lost item!");
+      root.insert(insertInterval, item);
+      System.out.println("reinsertion size = " + size());
+}
+    */
+    }
+
+    /**
+     * Removes a single item from the tree.
+     *
+     * @param itemEnv the Envelope of the item to be removed
+     * @param item the item to remove
+     * @return <code>true</code> if the item was found (and thus removed)
+     */
+    public boolean remove(Interval itemInterval, Object item)
+    {
+        Interval insertInterval = ensureExtent(itemInterval, minExtent);
+        return root.remove(insertInterval, item);
+    }
+
+    public Iterator iterator()
+    {
+        List foundItems = new ArrayList();
+        root.addAllItems(foundItems);
+        return foundItems.iterator();
+    }
+
+    public List query(double x)
+    {
+        return query(new Interval(x, x));
+    }
+
+    /**
+     * Queries the tree to find all candidate items which
+     * may overlap the query interval.
+     * If the query interval is <tt>null</tt>, all items in the tree are found.
+     *
+     * min and max may be the same value
+     */
+    public List query(Interval interval)
+    {
+        /**
+         * the items that are matched are all items in intervals
+         * which overlap the query interval
+         */
+        List foundItems = new ArrayList();
+        query(interval, foundItems);
+        return foundItems;
+    }
+
+    /**
+     * Adds items in the tree which potentially overlap the query interval
+     * to the given collection.
+     * If the query interval is <tt>null</tt>, add all items in the tree.
+     *
+     * @param interval a query nterval, or null
+     * @param resultItems the candidate items found
+     */
+    public void query(Interval interval, Collection foundItems)
+    {
+        root.addAllItemsFromOverlapping(interval, foundItems);
+    }
+
+    private void collectStats(Interval interval)
+    {
+        double del = interval.getWidth();
+        if (del < minExtent && del > 0.0)
+            minExtent = del;
+    }
+
 }
