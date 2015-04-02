d50521452fa31b7cd6d6f59df3b6556a892ab92f
David Smiley <david.w.smiley@gmail.com>
2015-04-02 09:15:31 -0400

    CQ8417: Update headers for MITRE / VoyagerSearch. Removed from pom.xml. Some files remain pending input from Eclipse Legal since they originate elsewhere: GeohashUtils, DistanceUtils, and ParseUtils WktShapeParser, PolygonBuilder, and WktShapeParserTest

--- src/test/java/org/locationtech/spatial4j/shape/RoundingDistCalc.java
+++ src/test/java/org/locationtech/spatial4j/shape/RoundingDistCalc.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Voyager Search and MITRE
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0 which
+ * accompanies this distribution and is available at
+ *    http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * Contributors:
+ *    Ryan McKinley - initial API and implementation
+ *    David Smiley
+ ******************************************************************************/
+
 package org.locationtech.spatial4j.shape;
 
 import org.locationtech.spatial4j.context.SpatialContext;
