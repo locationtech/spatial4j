a8c46b419d0d968723b60a4e4347555537340b86
jdeolive <jdeolive@gmail.com>
2015-04-17 10:37:07 -0600

    Update jeo to version 0.6.
    - Includes a package renaming from org.jeo to io.jeo
    - Artifacts also available in maven central to removing boundless
    repository reference.

--- src/test/java/com/spatial4j/core/io/GeoJSONReadWriteTest.java
+++ src/test/java/com/spatial4j/core/io/GeoJSONReadWriteTest.java
@@ -17,7 +17,7 @@ package com.spatial4j.core.io;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.jeo.geom.GeomBuilder;
+import io.jeo.geom.GeomBuilder;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
