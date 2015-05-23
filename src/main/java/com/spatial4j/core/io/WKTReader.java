174712ab21e827f15cd4d30701b925e3016bb39a
David Smiley <david.w.smiley@gmail.com>
2015-05-23 09:19:56 -0400

    CQ8417: Add headers for the files originating from Chris Male's fork of ElasticSearch: https://github.com/chrismale/elasticsearch  commit 14bc4dee08355048d6a94e33834b919a3999a06e They didn't have headers, but they were ASL licensed since the source codebase has that default license.
    (cherry picked from commit 52c43a39758b1493159cf506cf959f963df32907)
    
    # Conflicts:
    #	src/main/java/com/spatial4j/core/io/WKTReader.java

--- src/main/java/com/spatial4j/core/io/WKTReader.java
+++ src/main/java/com/spatial4j/core/io/WKTReader.java
@@ -1,20 +1,18 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+/*******************************************************************************
+ * Copyright (c) 2015 ElasticSearch and MITRE, and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0 which
+ * accompanies this distribution and is available at
+ *    http://www.apache.org/licenses/LICENSE-2.0.txt
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * Contributors:
+ *    Chris Male - initial API and implementation
+ *    David Smiley
+ *    Ryan McKinley
+ ******************************************************************************/
 
+// A derivative of commit 14bc4dee08355048d6a94e33834b919a3999a06e
+//  at https://github.com/chrismale/elasticsearch
 
 package com.spatial4j.core.io;
 
